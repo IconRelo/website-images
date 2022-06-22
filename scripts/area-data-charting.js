aeb8e7319e2fa48dba1237c10613d778509e8d34
shaunfl4 <59011283+shaunfl4@users.noreply.github.com>
2022-06-22 13:21:10 +0100

    Pie chart

--- scripts/area-data-charting.js
+++ scripts/area-data-charting.js
@@ -5,6 +5,7 @@
         chart['uk_stock'] = echarts.init(document.getElementById('uk-stock'));
         chart['regional_stock'] = echarts.init(document.getElementById('regional-stock'));
         chart['regional_pricing'] = echarts.init(document.getElementById('regional-pricing'));
+        chart['bedroom_breakdown'] = echarts.init(document.getElementById('bedroom-breakdown'));
 
 
   let populateData = (data) =>  {
@@ -12,7 +13,92 @@
     populateUKStock(data);
     populateRegionalStock(data.children);
     populateRegionalPricing(data.children);
+    populateBedroomBreakdown(data.data)
 
+  };
+
+  let populateBedroomBreakdown = (data) => {
+
+    let raw_data = [];
+
+    data.forEach((item) => {
+        item.items.forEach((bedroom) => {
+            raw_data.push(
+                {
+                    name: bedroom + ' bedroom',
+                    value: bedroom.count,
+                    date: Date.parse(bedroom.date)
+                }
+            )
+        })
+    });
+
+    console.log(raw_data);
+
+    /*[
+            ['product', '2012', '2013', '2014', '2015', '2016', '2017'],
+            ['Milk Tea', 56.5, 82.1, 88.7, 70.1, 53.4, 85.1],
+            ['Matcha Latte', 51.1, 51.4, 55.1, 53.3, 73.8, 68.7],
+            ['Cheese Cocoa', 40.1, 62.2, 69.5, 36.4, 45.2, 32.5],
+            ['Walnut Brownie', 25.2, 37.1, 41.2, 18, 33.9, 49.1]
+          ]*/
+
+    option = {
+        legend: {},
+        tooltip: {
+          trigger: 'axis',
+          showContent: false
+        },
+        dataset: {
+          source: raw_data
+        },
+        xAxis: { type: 'category' },
+        yAxis: { gridIndex: 0 },
+        grid: { top: '55%' },
+        series: [
+          {
+            type: 'line',
+            smooth: true,
+            seriesLayoutBy: 'row',
+            emphasis: { focus: 'series' }
+          },
+          {
+            type: 'line',
+            smooth: true,
+            seriesLayoutBy: 'row',
+            emphasis: { focus: 'series' }
+          },
+          {
+            type: 'line',
+            smooth: true,
+            seriesLayoutBy: 'row',
+            emphasis: { focus: 'series' }
+          },
+          {
+            type: 'line',
+            smooth: true,
+            seriesLayoutBy: 'row',
+            emphasis: { focus: 'series' }
+          },
+          {
+            type: 'pie',
+            id: 'pie',
+            radius: '30%',
+            center: ['50%', '25%'],
+            emphasis: {
+              focus: 'self'
+            },
+            label: {
+              formatter: '{b}: {@2012} ({d}%)'
+            },
+            encode: {
+              itemName: 'product',
+              value: '2012',
+              tooltip: '2012'
+            }
+          }
+        ]
+      };
   };
 
   let populateUKStock = (data) => {
@@ -42,12 +128,10 @@
   let populateRegionalStock = (data) => {
 
     let labels = data.map((item) => item.area);
-    console.log(labels)
     const datasetWithFilters = [];
     const seriesList = [];
     let raw_data = [];
     data.forEach((element) => {
-        console.log(element)
         let mapped_data = element.data.map((item) => {
             return {
                 'name':element.area,
@@ -163,12 +247,10 @@
   let populateRegionalPricing = (data) => {
 
     let labels = data.map((item) => item.area);
-    console.log(labels)
     const datasetWithFilters = [];
     const seriesList = [];
     let raw_data = [];
     data.forEach((element) => {
-        console.log(element)
         let mapped_data = element.data.map((item) => {
             return {
                 'name':element.area,
@@ -182,25 +264,24 @@
             raw_data.push(mapped_element)
         })
     })
-
-    console.log(raw_data);
     
 
-  echarts.util.each(labels, function (region) {
-    var datasetId = 'dataset_' + region;
-    datasetWithFilters.push({
-      id: datasetId,
-      fromDatasetId: 'dataset_raw',
-      transform: {
-        type: 'filter',
-        config: {
-          and: [
-            { dimension: 'date', gte: 1950 },
-            { dimension: 'region', '=': region }
-          ]
+    echarts.util.each(labels, function (region) {
+        var datasetId = 'dataset_' + region;
+        datasetWithFilters.push({
+        id: datasetId,
+        fromDatasetId: 'dataset_raw',
+        transform: {
+            type: 'filter',
+            config: {
+            and: [
+                { dimension: 'date', gte: 1950 },
+                { dimension: 'region', '=': region }
+            ]
+            }
         }
-      }
     });
+    
     seriesList.push({
       type: 'line',
       datasetId: datasetId,
