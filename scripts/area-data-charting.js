4bf7326202647868cc1de8c8e28a049a55a6b033
shaunfl4 <59011283+shaunfl4@users.noreply.github.com>
2022-06-22 11:51:15 +0100

    Initial regional charting

--- scripts/area-data-charting.js
+++ scripts/area-data-charting.js
@@ -3,15 +3,17 @@
  // Initialize the echarts instance based on the prepared dom
     let chart = []
         chart['uk_stock'] = echarts.init(document.getElementById('uk-stock'));
+        chart['regional_stock'] = echarts.init(document.getElementById('regional-stock'));
 
 
   let populateData = (data) =>  {
     console.log('is new:', data)
-    populateStock(data);
+    populateUKStock(data);
+    populateRegionalStock(data.children);
 
   };
 
-  let populateStock = (data) => {
+  let populateUKStock = (data) => {
 
     let chart_data = data.data.map((item) => item.stock);
     let labels = data.data.map((item) => item.items[0].date);
@@ -33,7 +35,86 @@
         ]
      })
 
-    
+  }
+
+  let populateRegionalStock = (data) => {
+
+    let labels = data.map((item) => item.area)
+
+  const datasetWithFilters = [];
+  const seriesList = [];
+
+  echarts.util.each(labels, function (region) {
+    var datasetId = 'dataset_' + region;
+    datasetWithFilters.push({
+      id: datasetId,
+      fromDatasetId: 'dataset_raw',
+      transform: {
+        type: 'filter',
+        config: {
+          and: [
+            { dimension: 'Year', gte: 1950 },
+            { dimension: 'Region', '=': region }
+          ]
+        }
+      }
+    });
+    seriesList.push({
+      type: 'line',
+      datasetId: datasetId,
+      showSymbol: false,
+      name: region,
+      endLabel: {
+        show: true,
+        formatter: function (params) {
+          return params.value[3] + ': ' + params.value[0];
+        }
+      },
+      labelLayout: {
+        moveOverlap: 'shiftY'
+      },
+      emphasis: {
+        focus: 'series'
+      },
+      encode: {
+        x: 'Date',
+        y: 'Stock',
+        label: ['Region', 'Stock'],
+        itemName: 'Date',
+        tooltip: ['Stock']
+      }
+    });
+  });
+  option = {
+    animationDuration: 10000,
+    dataset: [
+      {
+        id: 'dataset_raw',
+        source: data
+      },
+      ...datasetWithFilters
+    ],
+    title: {
+      text: 'Stock levels by region'
+    },
+    tooltip: {
+      order: 'valueDesc',
+      trigger: 'axis'
+    },
+    xAxis: {
+      type: 'time',
+      nameLocation: 'middle'
+    },
+    yAxis: {
+      name: 'Income'
+    },
+    grid: {
+      right: 140
+    },
+    series: seriesList
+  };
+  chart['regional_stock'].setOption(option);
+
   }
    
 fetch('https://area-data-api.test/api/places/8964/reports/yearly?date=2022-05-01&with_children=1', {
