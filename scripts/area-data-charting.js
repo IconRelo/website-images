a8d0021f7c57ba4e87d227c4731383c6e8b7dfb3
shaunfl4 <59011283+shaunfl4@users.noreply.github.com>
2022-06-22 11:37:43 +0100

    Adjust chart data

--- scripts/area-data-charting.js
+++ scripts/area-data-charting.js
@@ -1,4 +1,10 @@
 
+
+ // Initialize the echarts instance based on the prepared dom
+ let chart = []
+    chart['uk_stock'] = echarts.init(document.getElementById('uk-stock'));
+
+
   let populateData = (data) =>  {
     console.log('is new:', data)
     populateStock(data);
@@ -8,9 +14,16 @@
   let populateStock = (data) => {
 
     let chart_data = data.data.map((item) => item.stock);
+    let labels = data.data.map((item) => item.items[0].date);
 
     myChart.setOption({
-        // .... some configuration
+        xAxis: {
+            type: 'category',
+            data: labels
+          },
+          yAxis: {
+            type: 'value'
+          },
         series: [
             {
                 type: "line",
@@ -19,6 +32,8 @@
             }
         ]
      })
+
+    
   }
    
 fetch('https://area-data-api.test/api/places/8964/reports/yearly?date=2022-05-01&with_children=1', {
