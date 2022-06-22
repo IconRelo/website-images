f0b78d661f74bc808c4fe8859d28ecd66d26b2de
shaunfl4 <59011283+shaunfl4@users.noreply.github.com>
2022-06-22 13:04:29 +0100

    Add pricing

--- scripts/area-data-charting.js
+++ scripts/area-data-charting.js
@@ -4,12 +4,14 @@
     let chart = []
         chart['uk_stock'] = echarts.init(document.getElementById('uk-stock'));
         chart['regional_stock'] = echarts.init(document.getElementById('regional-stock'));
+        chart['regional_pricing'] = echarts.init(document.getElementById('regional-pricing'));
 
 
   let populateData = (data) =>  {
     console.log('is new:', data)
     populateUKStock(data);
     populateRegionalStock(data.children);
+    populateRegionalPricing(data.children);
 
   };
 
@@ -86,7 +88,6 @@
       endLabel: {
         show: true,
         formatter: function (params) {
-            console.log('params', params)
           return params.data.name + ': ' + params.data.value;
         }
       },
@@ -135,6 +136,130 @@
     dimensions: ['date', 'stock']
   };
   chart['regional_stock'].setOption(option);
+
+  }
+
+  let populateUKPricing = (data) => {
+
+    let chart_data = data.data.map((item) => item.stock);
+    let labels = data.data.map((item) => item.items[0].date);
+
+    chart['uk_stock'].setOption({
+        xAxis: {
+            type: 'category',
+            data: labels
+          },
+          yAxis: {
+            type: 'value'
+          },
+        series: [
+            {
+                type: "line",
+                name: "test",
+                data: chart_data
+            }
+        ]
+     })
+
+  }
+
+  let populateRegionalPricing = (data) => {
+
+    let labels = data.map((item) => item.area);
+    console.log(labels)
+    const datasetWithFilters = [];
+    const seriesList = [];
+    let raw_data = [];
+    data.forEach((element) => {
+        console.log(element)
+        let mapped_data = element.data.map((item) => {
+            return {
+                'name':element.area,
+                'region':element.area,
+                'value': item.stock,
+                'rent': item.average_rent_50,
+                'date': Date.parse(item.items[0].date)
+            }
+        })
+        mapped_data.forEach((mapped_element) => {
+            raw_data.push(mapped_element)
+        })
+    })
+
+    console.log(raw_data);
+    
+
+  echarts.util.each(labels, function (region) {
+    var datasetId = 'dataset_' + region;
+    datasetWithFilters.push({
+      id: datasetId,
+      fromDatasetId: 'dataset_raw',
+      transform: {
+        type: 'filter',
+        config: {
+          and: [
+            { dimension: 'date', gte: 1950 },
+            { dimension: 'region', '=': region }
+          ]
+        }
+      }
+    });
+    seriesList.push({
+      type: 'line',
+      datasetId: datasetId,
+      showSymbol: false,
+      name: region,
+      endLabel: {
+        show: true,
+        formatter: function (params) {
+          return params.data.name + ': ' + params.data.value;
+        }
+      },
+      labelLayout: {
+        moveOverlap: 'shiftY'
+      },
+      emphasis: {
+        focus: 'series'
+      },
+      encode: {
+        x: 'date',
+        y: 'rent',
+        label: ['region', 'rent'],
+        itemName: 'Date',
+        tooltip: ['rent']
+      }
+    });
+  });
+  option = {
+    animationDuration: 5000,
+    dataset: [
+      {
+        id: 'dataset_raw',
+        source: raw_data
+      },
+      ...datasetWithFilters
+    ],
+    title: {
+      text: 'Average Rent by region'
+    },
+    tooltip: {
+      order: 'valueDesc',
+      trigger: 'axis'
+    },
+    xAxis: {
+      type: 'time',
+      nameLocation: 'middle'
+    },
+    yAxis: {
+      name: 'Income'
+    },
+    grid: {
+      right: 140
+    },
+    series: seriesList,
+    dimensions: ['date', 'rent']
+  };
+  chart['regional_pricing'].setOption(option);
 
   }
    
